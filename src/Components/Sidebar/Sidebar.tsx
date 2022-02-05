e8e923686698db8d4d373bdd6fa6726b3e3b4b95
aegydm <aaaaydm3148@gmail.com>
2022-02-05 11:04:49 +0900

    라우터 설정

---
+++ src/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,47 @@
+import { NavLink } from "react-router-dom"
+import Box from "../Box"
+import "./style.css"
+
+
+const Sidebar = () => {
+    return (
+        <aside>
+            <Box 
+            isPadding
+            >
+                <header>
+                    <h1>
+                        CORONA LIVE
+                    </h1>
+                </header>
+            </Box>
+            <Box>
+            <ul>
+                <NavLink to="/" className={({
+                    isActive
+                }) => isActive ? 'selected' : ''}>
+                    <li>
+                        국내
+                    </li>
+                </NavLink>
+                <NavLink to="/world" className={({
+                    isActive
+                }) => isActive ? 'selected' : ''}>
+                    <li>
+                        해외
+                    </li>
+                </NavLink>
+                <NavLink to="/vaccine" className={({
+                    isActive
+                }) => isActive ? 'selected' : ''}>
+                    <li>
+                        백신
+                    </li>
+                </NavLink>
+            </ul>
+            </Box>
+        </aside>
+    )
+}
+
+export default Sidebar
---
+++ src/pages/Korea.tsx
@@ -0,0 +1,14 @@
+import Summary from "../Components/Summary"
+import TodaySummary from "../TodaySummary"
+
+
+const Korea = () => {
+    return (
+        <>
+            <Summary/>
+            <TodaySummary/>
+        </>
+    )
+}
+
+export default Korea
